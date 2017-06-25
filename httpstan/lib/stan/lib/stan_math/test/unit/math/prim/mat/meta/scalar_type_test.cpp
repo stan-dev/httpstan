4ec43b5fe2cc51f70b8db0f191a635d6ad90ec47
riddella <riddella>
2017-06-25 09:27:14 -0400

    Update Stan Math source to 2.16.0
    Neglected to update this earlier.

---
+++ httpstan/lib/stan/lib/stan_math/test/unit/math/prim/mat/meta/scalar_type_test.cpp
@@ -0,0 +1,19 @@
+#include <stan/math/prim/mat.hpp>
+#include <gtest/gtest.h>
+#include <test/unit/util.hpp>
+#include <vector>
+
+TEST(MetaTraits, ScalarTypeMat) {
+  using stan::scalar_type;
+  using Eigen::MatrixXd;
+  using Eigen::VectorXd;
+  using Eigen::RowVectorXd;
+  using test::expect_same_type;
+  using std::vector;
+
+  expect_same_type<double, scalar_type<MatrixXd>::type>();
+  expect_same_type<double, scalar_type<VectorXd>::type>();
+  expect_same_type<double, scalar_type<RowVectorXd>::type>();
+  expect_same_type<double, scalar_type<vector<double> >::type>();
+  expect_same_type<double, scalar_type<vector<MatrixXd> >::type>();
+}
