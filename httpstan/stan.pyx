d674b967d83d5ec99ce19032fc70af563ee766ad
ariddell <ariddell>
2017-07-03 17:19:30 -0400

    Add support multi-dimensional data.
    The helper function which converts multi-dimensional data into the
    flattened data (column-major) required by Stan is
    ``httpstan.utils._split_data``.
    
    Closes #1

--- httpstan/stan.pyx
+++ httpstan/stan.pyx
@@ -1,55 +1,16 @@
 # distutils: language=c++
 # cython: language_level=3
 """Wrap Stan classes and functions used by stan::services functions."""
+from cpython cimport pycapsule
 import numpy as np
 
-from cpython cimport pycapsule
-
 cimport httpstan.stan as stan
+import httpstan.utils
 
 
 def version() -> str:
     """Return Stan version."""
     return b'.'.join([stan.MAJOR_VERSION, stan.MINOR_VERSION, stan.PATCH_VERSION]).decode()
-
-
-def _split_data(dict data):
-    """Prepare data for use in an array_var_context constructor.
-
-    array_var_context is a C++ class defined in Stan. See
-    ``array_var_context.hpp`` for details.
-
-    The constructor signature is::
-
-        array_var_context(const std::vector<std::string>& names_r,
-                          const std::vector<double>& values_r,
-                          const std::vector<std::vector<size_t> >& dim_r,
-                          const std::vector<std::string>& names_i,
-                          const std::vector<int>& values_i,
-                          const std::vector<std::vector<size_t> >& dim_i)
-
-    """
-    data = data.copy()
-    names_r, values_r, dim_r = [], [], []
-    names_i, values_i, dim_i = [], [], []
-    for k, v in data.items():
-        if np.issubdtype(np.asarray(v).dtype, float):
-            names_r.append(k.encode('utf-8'))
-            values_r.extend(np.atleast_1d(v).astype(float))
-            # TODO(AR): implement support for higher dimensional data
-            if np.asarray(v).ndim > 1:
-                raise NotImplementedError('Higher dimensional variables not yet supported.')
-            dim_r.append(np.asarray(v).shape)
-        elif np.issubdtype(np.asarray(v).dtype, np.integer):
-            names_i.append(k.encode('utf-8'))
-            values_i.extend(np.atleast_1d(v).astype(int))
-            # TODO(AR): implement support for higher dimensional data
-            if np.asarray(v).ndim > 1:
-                raise NotImplementedError('Higher dimensional variables not yet supported.')
-            dim_i.append(np.asarray(v).shape)
-        else:
-            raise ValueError(f'Variable `{k}` must be int or float.')
-    return names_r, values_r, dim_r, names_i, values_i, dim_i
 
 
 cdef void del_array_var_context(object obj):
@@ -64,7 +25,7 @@ def make_array_var_context(dict data):
     See the C++ documentation for array_var_context for details.
 
     """
-    names_r_, values_r_, dim_r_, names_i_, values_i_, dim_i_ = _split_data(data)
+    names_r_, values_r_, dim_r_, names_i_, values_i_, dim_i_ = httpstan.utils._split_data(data)
     cdef vector[string] names_r = names_r_
     cdef vector[double] values_r = values_r_
     cdef vector[vector[size_t]] dim_r = dim_r_
---
+++ tests/test_linear_regression.py
@@ -0,0 +1,75 @@
+"""Test sampling from linear regression model."""
+import json
+
+import aiohttp
+import numpy as np
+
+
+host, port = '127.0.0.1', 8080
+programs_url = 'http://{}:{}/v1/programs'.format(host, port)
+headers = {'content-type': 'application/json'}
+program_code = """
+    data {
+        int<lower=0> N;
+        int<lower=0> p;
+        matrix[N,p] x;
+        vector[N] y;
+    }
+    parameters {
+        vector[p] beta;
+        real<lower=0> sigma;
+    }
+    model {
+        y ~ normal(x * beta, sigma);
+    }
+"""
+np.random.seed(1)
+
+n = 10000
+p = 3
+
+beta_true = beta_true = (1, 3, 5)
+X = np.random.normal(size=(n, p))
+X = (X - np.mean(X, axis=0)) / np.std(X, ddof=1, axis=0, keepdims=True)
+y = np.dot(X, beta_true) + np.random.normal(size=n)
+data = {'N': n, 'p': p, 'x': X.tolist(), 'y': y.tolist()}
+
+
+def test_linear_regression(loop_with_server):
+    """Test sampling from linear regression posterior with defaults."""
+    async def main():
+        async with aiohttp.ClientSession() as session:
+            payload = {'program_code': program_code}
+            async with session.post(programs_url, data=json.dumps(payload), headers=headers) as resp:
+                assert resp.status == 200
+                program_id = (await resp.json())['id']
+
+            programs_actions_url = 'http://{}:{}/v1/programs/{}/actions'.format(host, port, program_id)
+            payload = {
+                'type': 'stan::services::sample::hmc_nuts_diag_e_adapt',
+                'data': data,
+                'num_samples': 500,
+                'num_warmup': 500,
+            }
+
+            # XXX: this function is repeated from programs_actions
+            draws = []
+            async with session.post(programs_actions_url, data=json.dumps(payload), headers=headers) as resp:
+                assert resp.status == 200
+                while True:
+                    chunk = await resp.content.readline()
+                    if not chunk:
+                        break
+                    assert len(chunk)
+                    payload = json.loads(chunk)
+                    assert len(payload) > 0
+                    if payload['topic'] == 'SAMPLE':
+                        assert isinstance(payload['feature'], dict)
+                        if 'beta.1' in payload['feature']:
+                            draws.append(payload['feature'])
+
+            assert len(draws) > 0
+            beta_0 = np.array([draw['beta.1']['doubleList']['value'].pop() for draw in draws])
+            assert all(np.abs(beta_0 - np.array(beta_true)[0]) < 0.05)
+
+    loop_with_server.run_until_complete(main())
