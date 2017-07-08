055dfaf6efecdf37fbde799ce7441f0ccfc0bf08
ariddell <ariddell>
2017-07-07 20:15:13 -0400

    Remove parallel sampling test. No support yet.

--- tests/test_bernoulli.py
+++ tests/test_bernoulli.py
@@ -54,36 +54,3 @@ def test_bernoulli(loop_with_server, host, port):
                 await validate_samples(resp)
 
     loop_with_server.run_until_complete(main())
-
-
-def test_bernoulli_parallel(loop_with_server, host, port):
-    """Test sampling from Bernoulli program in parallel."""
-    async def main():
-        async with aiohttp.ClientSession() as session:
-            programs_url = 'http://{}:{}/v1/programs'.format(host, port)
-            payload = {'program_code': program_code}
-            async with session.post(programs_url, data=json.dumps(payload), headers=headers) as resp:
-                assert resp.status == 200
-                program_id = (await resp.json())['id']
-
-        programs_actions_url = 'http://{}:{}/v1/programs/{}/actions'.format(host, port, program_id)
-        payload = {'type': 'stan::services::sample::hmc_nuts_diag_e_adapt', 'data': data}
-
-        # record time for sampling
-        # this must be run in an executor because loop is managing the server
-        request_function = functools.partial(requests.post, programs_actions_url, json=payload)
-        t0 = time.time()
-        loop = asyncio.get_event_loop()
-        r = await loop.run_in_executor(None, request_function)
-        elapsed = time.time() - t0
-
-        # run many requests in parallel
-        num_threads = 4
-        t0 = time.time()
-        futs = [asyncio.ensure_future(loop.run_in_executor(None, request_function)) for _ in range(num_threads)]
-        for fut in futs:
-            r = await fut
-            assert r.text
-        assert (time.time() - t0) < num_threads * elapsed
-
-    loop_with_server.run_until_complete(main())
