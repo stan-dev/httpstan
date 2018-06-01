95169fa2a56862b9e42a34cb92f4c1461542ee62
ariddell <ariddell>
2018-06-01 15:43:03 -0400

    Update include dirs for new version of Stan

--- httpstan/models.py
+++ httpstan/models.py
@@ -191,10 +191,8 @@ def _build_extension_module(
             os.path.join(httpstan_dir, "lib", "stan", "src"),
             os.path.join(httpstan_dir, "lib", "stan", "lib", "stan_math"),
             os.path.join(httpstan_dir, "lib", "stan", "lib", "stan_math", "lib", "eigen_3.3.3"),
-            os.path.join(httpstan_dir, "lib", "stan", "lib", "stan_math", "lib", "boost_1.64.0"),
-            os.path.join(
-                httpstan_dir, "lib", "stan", "lib", "stan_math", "lib", "cvodes_2.9.0", "include"
-            ),
+            os.path.join(httpstan_dir, "lib", "stan", "lib", "stan_math", "lib", "boost_1.66.0"),
+            os.path.join(httpstan_dir, "lib", "stan", "lib", "stan_math", "lib", "sundials_3.1.0", "include"),
         ]
 
         stan_macros: List[Tuple[str, Optional[str]]] = [
