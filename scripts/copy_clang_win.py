69439aa1a4b1828c54ccbcad6891c4fbb93b9bf6
Hartikainen Ari <ari.hartikainen@aalto.fi>
2018-07-29 21:26:54 +0300

    overwrite cl.exe

--- scripts/copy_clang_win.py
+++ scripts/copy_clang_win.py
@@ -4,7 +4,8 @@ import shutil
 # copy clang-cl.exe to cl.exe
 src = os.popen("where clang-cl.exe").read().strip().splitlines()[0].strip()
 src = os.path.normpath(os.path.abspath(src))
-dst, _ = os.path.split(src)
-dst = os.path.join(dst, "cl.exe")
+#dst, _ = os.path.split(src)
+#dst = os.path.join(dst, "cl.exe")
+dst = "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\BIN\\x86_amd64\\cl.exe"
 shutil.copy2(src, dst)
 print("copied {} to {}".format(src, dst))
