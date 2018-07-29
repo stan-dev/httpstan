ad0e186eb8f52c4046e3238b4db0c4206578f1b2
Hartikainen Ari <ari.hartikainen@aalto.fi>
2018-07-29 19:08:56 +0300

    choose first clang

--- scripts/copy_clang_win.py
+++ scripts/copy_clang_win.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 # copy clang-cl.exe to cl.exe
-src = os.popen("where clang-cl.exe").read().strip()
+src = os.popen("where clang-cl.exe").read().strip().splitlines()[0].strip()
 src = os.path.normpath(os.path.abspath(src))
 dst, _ = os.path.split(src)
 dst = os.path.join(dst, "cl.exe")
