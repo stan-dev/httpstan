d852506aa77fd243566de62ab083d2eed7f613a1
Allen Riddell <riddella+stan@indiana.edu>
2018-12-03 16:17:46 -0500

    fix: pass random seed to C++ model constructor
    Copy-and-paste humor error. Consider code generation for
    most if not all of `anonymous_stan_model_services.pyx.template`.
    
    Also ports several PyStan 2 tests.

--- tests/test_fits.py
+++ tests/test_fits.py
@@ -2,6 +2,7 @@
 import asyncio
 import statistics
 
+import numpy as np
 import requests
 
 import helpers
@@ -42,6 +43,7 @@ def test_fits(api_url):
 
         fit_url = f"{api_url}/{fit_name}"
         resp = requests.get(fit_url)
+        assert resp.status_code == 200
         fit_bytes = resp.content
         draws = helpers.extract_draws(fit_bytes, "y")
         assert len(draws) == num_samples, (len(draws), num_samples)
@@ -61,3 +63,47 @@ def test_models_actions_bad_args(api_url):
         assert resp.json() == {"function": ["Missing data for required field."]}
 
     asyncio.get_event_loop().run_until_complete(main(asyncio.get_event_loop()))
+
+
+def test_fits_random_seed(api_url):
+    """Simple test of sampling with fixed random seed."""
+
+    async def draws(random_seed=None):
+        model_name = helpers.get_model_name(api_url, program_code)
+        fits_url = f"{api_url}/models/{model_name.split('/')[-1]}/fits"
+        data = {"function": "stan::services::sample::hmc_nuts_diag_e_adapt"}
+        if random_seed is not None:
+            data["random_seed"] = random_seed
+        resp = requests.post(fits_url, json=data)
+        assert resp.status_code == 201
+        operation = resp.json()
+        operation_name = operation["name"]
+        fit_name = operation["metadata"]["fit"]["name"]
+
+        resp = requests.get(f"{api_url}/{operation_name}")
+        assert resp.status_code == 200, f"{api_url}/{operation_name}"
+
+        # wait until fit is finished
+        while not requests.get(f"{api_url}/{operation_name}").json()["done"]:
+            await asyncio.sleep(0.1)
+
+        fit_url = f"{api_url}/{fit_name}"
+        resp = requests.get(fit_url)
+        assert resp.status_code == 200
+        fit_bytes = resp.content
+        return helpers.extract_draws(fit_bytes, "y")
+
+    async def main():
+        draws1 = np.array(await draws(random_seed=123))
+        draws2 = np.array(await draws(random_seed=123))
+        draws3 = np.array(await draws(random_seed=456))
+        draws4 = np.array(await draws())
+
+        assert draws1[0] == draws2[0] != draws4[0]
+        assert draws2[0] != draws4[0]
+        assert draws1[0] != draws3[0] != draws4[0]
+        # look at all draws
+        assert np.allclose(draws1, draws2)
+        assert not np.allclose(draws1, draws3)
+
+    asyncio.get_event_loop().run_until_complete(main())
---
+++ tests/test_generated_quantities_rng.py
@@ -0,0 +1,60 @@
+"""Test generated quantities rng."""
+import asyncio
+import requests
+
+import numpy as np
+
+import helpers
+
+
+program_code = """
+    parameters {
+      real z;
+    }
+    model {
+      z ~ normal(0, 1);
+    }
+    generated quantities {
+      real y = normal_rng(0, 1);
+    }
+"""
+
+
+def test_generated_quantities_rng(api_url):
+    """Test consistency in rng in `generated quantities` block."""
+
+    async def draws(random_seed):
+        model_name = helpers.get_model_name(api_url, program_code)
+        fits_url = f"{api_url}/models/{model_name.split('/')[-1]}/fits"
+        data = {
+            "function": "stan::services::sample::hmc_nuts_diag_e_adapt",
+            "random_seed": random_seed,
+        }
+        resp = requests.post(fits_url, json=data)
+        assert resp.status_code == 201
+        operation = resp.json()
+        operation_name = operation["name"]
+        fit_name = operation["metadata"]["fit"]["name"]
+
+        resp = requests.get(f"{api_url}/{operation_name}")
+        assert resp.status_code == 200, f"{api_url}/{operation_name}"
+
+        # wait until fit is finished
+        while not requests.get(f"{api_url}/{operation_name}").json()["done"]:
+            await asyncio.sleep(0.1)
+
+        fit_url = f"{api_url}/{fit_name}"
+        resp = requests.get(fit_url)
+        fit_bytes = resp.content
+        return helpers.extract_draws(fit_bytes, "y")
+
+    async def main():
+        draws1 = np.array(await draws(random_seed=1))
+        draws2 = np.array(await draws(random_seed=1))
+        draws3 = np.array(await draws(random_seed=2))
+        assert len(draws1) == len(draws2) == len(draws3)
+        assert len(draws1) >= 1000
+        assert np.allclose(draws1, draws2)
+        assert not np.allclose(draws1, draws3)
+
+    asyncio.get_event_loop().run_until_complete(main())
