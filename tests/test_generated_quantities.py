31a0076ab50db6902b8d5b5171699c7c6993a9fc
Allen Riddell <riddella+stan@indiana.edu>
2018-12-03 16:17:46 -0500

    tests: add generated quantities tests

---
+++ tests/test_generated_quantities.py
@@ -0,0 +1,48 @@
+"""Superficial test of `generated quantities` block."""
+import asyncio
+
+import numpy as np
+import requests
+
+import helpers
+
+program_code = """
+    parameters {
+      real y;
+    }
+    model {
+      y ~ normal(0, 1);
+    }
+    generated quantities {
+        real y_new = y + 3;
+    }
+"""
+
+
+def test_generated_quantities_block(api_url):
+    """Test `generated_quantities` block."""
+
+    async def main():
+        model_name = helpers.get_model_name(api_url, program_code)
+        fits_url = f"{api_url}/models/{model_name.split('/')[-1]}/fits"
+        data = {"function": "stan::services::sample::hmc_nuts_diag_e_adapt"}
+        resp = requests.post(fits_url, json=data)
+        assert resp.status_code == 201
+        operation = resp.json()
+        operation_name = operation["name"]
+
+        fit_name = operation["metadata"]["fit"]["name"]
+        resp = requests.get(f"{api_url}/{operation_name}")
+
+        # wait until fit is finished
+        while not requests.get(f"{api_url}/{operation_name}").json()["done"]:
+            await asyncio.sleep(0.1)
+
+        fit_url = f"{api_url}/{fit_name}"
+        resp = requests.get(fit_url)
+        fit_bytes = resp.content
+        y = np.array(helpers.extract_draws(fit_bytes, "y"))
+        y_new = np.array(helpers.extract_draws(fit_bytes, "y_new"))
+        np.testing.assert_allclose(y + 3, y_new, atol=0.001)
+
+    asyncio.get_event_loop().run_until_complete(main())
