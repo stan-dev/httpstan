dbff621decd92f4433cac5aca65d735c22ece57a
Allen Riddell <riddella+stan@indiana.edu>
2018-12-26 10:23:15 -0500

    build: Script which automates updating Stan source

---
+++ scripts/update_stan_source.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+err() {
+    echo "$@" >&2
+}
+
+if [[ $# -ne 1 ]] ; then
+    err "Provide upstream Stan tag (e.g., 'v2.18.1')"
+    exit 1
+fi
+
+TAG="$@"
+VERSION=$( echo "$TAG" | sed 's/^v//' )
+echo "Updating Stan source to tag: ${TAG}"
+read -p "Ok to continue (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        echo "Continuing..."
+    ;;
+    * )
+        err "Not updating Stan source"
+        exit 1
+    ;;
+esac
+
+TEMPDIR=$(mktemp -d)
+echo "in script proper"
+curl -L "https://github.com/stan-dev/stan/archive/${TAG}.tar.gz" | \
+    tar -C "${TEMPDIR}" -zxvf -
+curl -L "https://github.com/stan-dev/math/archive/${TAG}.tar.gz" | \
+    tar -C "${TEMPDIR}" -zxvf -
+
+echo "using temporary directory: ${TEMPDIR}"
+
+pushd "${TEMPDIR}"
+rmdir "stan-${VERSION}/lib/stan_math"
+mv "${TEMPDIR}/math-${VERSION}" "${TEMPDIR}/stan-${VERSION}/lib/stan_math"
+popd
+
+
+echo "Running 'git stash'"
+git stash
+rm -rf httpstan/lib/stan
+mv "${TEMPDIR}/stan-${VERSION}" httpstan/lib/stan
+echo "Running 'git add httpstan/lib/stan -f'"
+git add httpstan/lib/stan -f
+echo "Running 'git commit' with message:"\
+    "'chore: Update Stan source to tag ${TAG}'"
+git commit -m"chore: Update Stan source to tag ${TAG}"
