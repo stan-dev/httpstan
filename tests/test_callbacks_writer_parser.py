411f19b77c5273c794cd1926f949c7b5a3a88200
Hartikainen Ari <ari.hartikainen@aalto.fi>
2019-08-08 00:53:13 +0300

    ignore numerical inaccuracy in callback parser

--- tests/test_callbacks_writer_parser.py
+++ tests/test_callbacks_writer_parser.py
@@ -1,5 +1,8 @@
 """Test parser for output of stan::services functions."""
 import json
+import re
+
+from numpy.testing import assert_almost_equal
 
 import httpstan.callbacks_writer_parser
 import httpstan.callbacks_writer_pb2 as callbacks_writer_pb2
@@ -45,10 +48,32 @@ def test_callbacks_writer_parser_sample_writer():
 
     parser = httpstan.callbacks_writer_parser.WriterParser()
     observed = [parser.parse(message) for message in messages]
+
     message_pb = callbacks_writer_pb2.WriterMessage(topic=TopicEnum.Value("SAMPLE"))
     for key, value in zip(sample_fields, values):
         feature = message_pb.feature.add()
         feature.name = key
         feature.double_list.value.append(value)
     expected = [None, message_pb]
-    assert observed == expected
+
+    # None == None
+    assert observed[0] == expected[0]
+
+    # 'callbacks_writer_pb2.WriterMessage'
+    assert type(observed[0]) == type(expected[0])  # noqa: disable=E721
+
+    # handle number and structure
+
+    # pattern for a number
+    pattern = r"[-+]*\d+[.]*\d*e*[-+]*\d*"
+    observed_num = [float(num) for num in re.findall(pattern, str(observed[1]))]
+    expected_num = [float(num) for num in re.findall(pattern, str(expected[1]))]
+
+    # Ignore floating point error
+    assert_almost_equal(observed_num, expected_num)
+
+    # replace numbers with text 'NUM'
+    observed_structure = re.sub(pattern, "NUM", str(observed[1]))
+    expected_structure = re.sub(pattern, "NUM", str(expected[1]))
+
+    assert observed_structure == expected_structure
