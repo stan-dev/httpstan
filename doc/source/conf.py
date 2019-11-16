50294d604ab032f1fb9b6ca268ac934d34bc17e5
Allen Riddell <riddella+stan@indiana.edu>
2019-11-15 20:13:31 -0500

    docs: Generate files in Sphinx conf.py
    Also turns on CI for docs.
    
    Closes #255

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,30 +1,26 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 import httpstan
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.openapi',
-    'sphinxcontrib.redoc',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinxcontrib.openapi",
+    "sphinxcontrib.redoc",
 ]
 
 redoc = [
-    {
-        'name': 'httpstan API',
-        'page': 'api',
-        'spec': 'openapi.yaml',
-    },
+    {"name": "httpstan API", "page": "api", "spec": "openapi.yaml",},
 ]
 
-source_suffix = '.rst'
-master_doc = 'index'
-project = u'httpstan'
-copyright = u'2019, httpstan Developers'
+source_suffix = ".rst"
+master_doc = "index"
+project = u"httpstan"
+copyright = u"2019, httpstan Developers"
 
 
 intersphinx_mapping = {
@@ -32,6 +28,56 @@ intersphinx_mapping = {
 }
 
 version = release = httpstan.__version__
+
+################################################################################
+# apidoc and openapi spec
+################################################################################
+
+# the following "hook" arranges for the equivalent of the following to be run:
+# sphinx-apidoc --ext-autodoc --force --no-toc -o doc/source httpstan httpstan/views.py httpstan/routes.py
+
+
+def run_apidoc(_):
+
+    output_path = os.path.join("doc", "source")
+    # excluding ``httpstan/views.py`` as Sphinx cannot process the OpenAPI YAML
+    # excluding ``httpstan/routes.py`` as useless without ``views.py``
+    ignored_files = [
+        "setup.py",
+        "httpstan/views.py",
+        "httpstan/routes.py",
+    ]
+    argv = [
+        "--ext-autodoc",
+        "--force",
+        "--no-toc",
+        "-o",
+        output_path,
+        os.path.join("..", project.lower()),
+    ] + ignored_files
+
+    from sphinx.ext import apidoc
+
+    apidoc.main(argv)
+
+
+def make_openapi_spec(_):
+
+    output_path = os.path.join("doc", "source", "openapi.yaml")
+    from httpstan import openapi
+
+    with open(output_path, "w") as fh:
+        fh.write(openapi.openapi_spec().to_yaml())
+
+
+def setup(app):
+    app.connect("builder-inited", run_apidoc)
+    app.connect("builder-inited", make_openapi_spec)
+
+
+################################################################################
+# theme configuration
+################################################################################
 
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
