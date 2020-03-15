550438d5361a1f18e946b9f3e3cdb03997610f27
Allen Riddell <riddella+stan@indiana.edu>
2020-03-14 21:04:38 -0400

    style: run cpplint

--- httpstan/queue_writer.hpp
+++ httpstan/queue_writer.hpp
@@ -1,10 +1,10 @@
-#ifndef STAN_CALLBACKS_QUEUE_WRITER_HPP
-#define STAN_CALLBACKS_QUEUE_WRITER_HPP
+#ifndef HTTPSTAN_QUEUE_WRITER_HPP
+#define HTTPSTAN_QUEUE_WRITER_HPP
 
-#include <boost/lockfree/spsc_queue.hpp>
-#include <stan/callbacks/writer.hpp>
 #include <vector>
 #include <string>
+#include <boost/lockfree/spsc_queue.hpp>
+#include <stan/callbacks/writer.hpp>
 #include "callbacks_writer.pb.h"
 
 /**
@@ -44,231 +44,228 @@
  *
  */
 
-
 namespace stan {
-  namespace callbacks {
+namespace callbacks {
 
-    /* Enum used by sample writer only. Keeps track of state. */
-    enum class ProcessingAdaptationState
-    {
-        BEFORE_PROCESSING_ADAPTATION, // if no adaptation, stay here
-        PROCESSING_ADAPTATION,
-        FINAL_ADAPTATION_MESSAGE,
-        AFTER_PROCESSING_ADAPTATION
-    };
+/* Enum used by sample writer only. Keeps track of state. */
+enum class ProcessingAdaptationState {
+  BEFORE_PROCESSING_ADAPTATION,  // if no adaptation, stay here
+  PROCESSING_ADAPTATION,
+  FINAL_ADAPTATION_MESSAGE,
+  AFTER_PROCESSING_ADAPTATION
+};
 
-    /**
-     * <code>queue_writer</code> is an implementation
-     * of <code>writer</code> that writes Protobuf-encoded values to a queue.
-     */
-    class queue_writer : public writer {
-    public:
-      /**
-       * Constructs a writer with an output queue
-       * and an optional prefix for comments.
-       *
-       * @param[in, out] output queues to write
-       * @param[in] message_prefix will be prefixed to each string which is added to the queue. Default is "".
-       */
-      queue_writer(boost::lockfree::spsc_queue<std::string> * output, const std::string& message_prefix = ""):
-        output_(output), message_prefix_(message_prefix) {}
+/**
+ * <code>queue_writer</code> is an implementation
+ * of <code>writer</code> that writes Protobuf-encoded values to a queue.
+ */
+class queue_writer : public writer {
+ public:
+  /**
+   * Constructs a writer with an output queue
+   * and an optional prefix for comments.
+   *
+   * @param[in, out] output queues to write
+   * @param[in] message_prefix will be prefixed to each string which is added to the queue. Default is "".
+   */
+  explicit queue_writer(boost::lockfree::spsc_queue<std::string> * output, const std::string& message_prefix = ""):
+    output_(output), message_prefix_(message_prefix) {}
 
-      /**
-       * Virtual destructor
-       */
-      virtual ~queue_writer() {}
+  /**
+   * Virtual destructor
+   */
+  virtual ~queue_writer() {}
 
-      /**
-       * Writes a sequence of names.
-       *
-       * @param[in] names Names in a std::vector
-       */
-      void operator()(const std::vector<std::string>& names) {
-        std::vector<std::string>::const_iterator last = names.end();
-        if (message_prefix_ == "diagnostic_writer:") {
-          if (diagnostic_fields_.empty()) {
-            for (std::vector<std::string>::const_iterator it = names.begin();
-                it != last; ++it) {
-              diagnostic_fields_.push_back(*it);
-            }
-            return;
-          }
-          stan::WriterMessage writer_message;
-          writer_message.set_topic(stan::WriterMessage_Topic_DIAGNOSTIC);
-
-          stan::WriterMessage_Feature * feature = writer_message.add_feature();
-          stan::WriterMessage_StringList * string_list = new stan::WriterMessage_StringList;
-          for (std::vector<std::string>::const_iterator it = names.begin();
-               it != last; ++it) {
-            string_list->add_value(*it);
-          }
-          feature->set_allocated_string_list(string_list);
-
-          std::string serialized;
-          writer_message.SerializeToString(&serialized);
-          output_->push(serialized);
-          return;
-        } else if (message_prefix_ == "init_writer:") {
-          throw std::runtime_error("Unexpected string vector for init writer.");
-        } else if (message_prefix_ == "sample_writer:") {
-          // sample writer receives only one string vector message, the column header
-          if (!sample_fields_.empty())
-            throw std::runtime_error("Unexpected string vector in sample writer after column header.");
-          for (std::vector<std::string>::const_iterator it = names.begin();
-               it != last; ++it) {
-            sample_fields_.push_back(*it);
-          }
-          return;
+  /**
+   * Writes a sequence of names.
+   *
+   * @param[in] names Names in a std::vector
+   */
+  void operator()(const std::vector<std::string>& names) {
+    std::vector<std::string>::const_iterator last = names.end();
+    if (message_prefix_ == "diagnostic_writer:") {
+      if (diagnostic_fields_.empty()) {
+        for (std::vector<std::string>::const_iterator it = names.begin();
+            it != last; ++it) {
+          diagnostic_fields_.push_back(*it);
         }
-      }
-
-      /**
-       * Writes a set of values.
-       *
-       * @param[in] state Values in a std::vector
-       */
-      void operator()(const std::vector<double>& state) {
-        std::vector<double>::const_iterator last = state.end();
-
-        if (message_prefix_ == "diagnostic_writer:") {
-          if (diagnostic_fields_.empty()) {
-            throw std::runtime_error("diagnostic fields must be set before receiving values");
-          }
-
-          stan::WriterMessage writer_message;
-          writer_message.set_topic(stan::WriterMessage_Topic_DIAGNOSTIC);
-          for (std::size_t i = 0; i < diagnostic_fields_.size(); ++i) {
-            stan::WriterMessage_Feature * feature = writer_message.add_feature();
-            feature->set_name(diagnostic_fields_[i]);
-            stan::WriterMessage_DoubleList * double_list = new stan::WriterMessage_DoubleList;
-            double_list->add_value(state[i]);
-            feature->set_allocated_double_list(double_list);
-          }
-
-          std::string serialized;
-          writer_message.SerializeToString(&serialized);
-          output_->push(serialized);
-          return;
-        } else if (message_prefix_ == "init_writer:") {
-          stan::WriterMessage writer_message;
-          writer_message.set_topic(stan::WriterMessage_Topic_INITIALIZATION);
-
-          stan::WriterMessage_Feature * feature = writer_message.add_feature();
-          stan::WriterMessage_DoubleList * double_list = new stan::WriterMessage_DoubleList;
-          for (std::vector<double>::const_iterator it = state.begin();
-              it != last; ++it) {
-            double_list->add_value(*it);
-          }
-          feature->set_allocated_double_list(double_list);
-          std::string serialized;
-          writer_message.SerializeToString(&serialized);
-          output_->push(serialized);
-          return;
-        } else if (message_prefix_ == "sample_writer:") {
-
-          if (sample_fields_.empty())
-            throw std::runtime_error("Sample fields should be populated before sample writer writes a vector of doubles.");
-
-          if ((processing_adaptation_state_ == ProcessingAdaptationState::PROCESSING_ADAPTATION) ||
-              (processing_adaptation_state_ == ProcessingAdaptationState::FINAL_ADAPTATION_MESSAGE))
-            throw std::runtime_error("Adaptation should have completed before sample writer writes a vector of doubles.");
-
-          stan::WriterMessage writer_message;
-          writer_message.set_topic(stan::WriterMessage_Topic_SAMPLE);
-
-          for (std::size_t i = 0; i < sample_fields_.size(); ++i) {
-            stan::WriterMessage_Feature * feature = writer_message.add_feature();
-            feature->set_name(sample_fields_[i]);
-            stan::WriterMessage_DoubleList * double_list = new stan::WriterMessage_DoubleList;
-            double_list->add_value(state[i]);
-            feature->set_allocated_double_list(double_list);
-          }
-          std::string serialized;
-          writer_message.SerializeToString(&serialized);
-          output_->push(serialized);
-          return;
-        }
-      }
-
-      /**
-       * Writes the message_prefix to the stream followed by a newline.
-       */
-      void operator()() {
-        // unused
         return;
       }
+      stan::WriterMessage writer_message;
+      writer_message.set_topic(stan::WriterMessage_Topic_DIAGNOSTIC);
 
-      /**
-       * Writes the message_prefix then the message.
-       *
-       * @param[in] message A string
-       */
-      void operator()(const std::string& message) {
-        if (message_prefix_ == "diagnostic_writer:") {
-          // DEBUG: prototype here
-          stan::WriterMessage writer_message;
-          writer_message.set_topic(stan::WriterMessage_Topic_DIAGNOSTIC);
+      stan::WriterMessage_Feature * feature = writer_message.add_feature();
+      stan::WriterMessage_StringList * string_list = new stan::WriterMessage_StringList;
+      for (std::vector<std::string>::const_iterator it = names.begin();
+           it != last; ++it) {
+        string_list->add_value(*it);
+      }
+      feature->set_allocated_string_list(string_list);
 
-          stan::WriterMessage_Feature * feature = writer_message.add_feature();
-          stan::WriterMessage_StringList * string_list = new stan::WriterMessage_StringList;
-          string_list->add_value(message);
-          feature->set_allocated_string_list(string_list);
+      std::string serialized;
+      writer_message.SerializeToString(&serialized);
+      output_->push(serialized);
+      return;
+    } else if (message_prefix_ == "init_writer:") {
+      throw std::runtime_error("Unexpected string vector for init writer.");
+    } else if (message_prefix_ == "sample_writer:") {
+      // sample writer receives only one string vector message, the column header
+      if (!sample_fields_.empty())
+        throw std::runtime_error("Unexpected string vector in sample writer after column header.");
+      for (std::vector<std::string>::const_iterator it = names.begin();
+           it != last; ++it) {
+        sample_fields_.push_back(*it);
+      }
+      return;
+    }
+  }
 
-          std::string serialized;
-          writer_message.SerializeToString(&serialized);
-          output_->push(serialized);
-          return;
-        } else if (message_prefix_ == "init_writer:") {
-          throw std::runtime_error("Unexpected string vector for init writer.");
-        } else if (message_prefix_ == "sample_writer:") {
-          // state machine dance here
-          if (processing_adaptation_state_ == ProcessingAdaptationState::BEFORE_PROCESSING_ADAPTATION) {
-            if (message.rfind("Adaptation terminated", 0) == 0) {
-              // message starts with "Adaptation terminated"
-              processing_adaptation_state_ = ProcessingAdaptationState::PROCESSING_ADAPTATION;
-            }
-          } else if (processing_adaptation_state_ == ProcessingAdaptationState::PROCESSING_ADAPTATION) {
-            if (message.rfind("Diagonal elements of inverse mass matrix", 0) == 0) {
-              // message starts with "Diagonal elements of inverse mass matrix"
-              // the next "message" (vector of doubles) will be the final adaptation message
-              processing_adaptation_state_ = ProcessingAdaptationState::FINAL_ADAPTATION_MESSAGE;
-            }
-          } else if (processing_adaptation_state_ == ProcessingAdaptationState::FINAL_ADAPTATION_MESSAGE) {
-            // this message is the last adaptation-related message before normal draws start arriving
-            processing_adaptation_state_ = ProcessingAdaptationState::AFTER_PROCESSING_ADAPTATION;
-          }
+  /**
+   * Writes a set of values.
+   *
+   * @param[in] state Values in a std::vector
+   */
+  void operator()(const std::vector<double>& state) {
+    std::vector<double>::const_iterator last = state.end();
 
-          stan::WriterMessage writer_message;
-          writer_message.set_topic(stan::WriterMessage_Topic_SAMPLE);
-
-          stan::WriterMessage_Feature * feature = writer_message.add_feature();
-          stan::WriterMessage_StringList * string_list = new stan::WriterMessage_StringList;
-          string_list->add_value(message);
-          feature->set_allocated_string_list(string_list);
-
-          std::string serialized;
-          writer_message.SerializeToString(&serialized);
-          output_->push(serialized);
-          return;
-        }
+    if (message_prefix_ == "diagnostic_writer:") {
+      if (diagnostic_fields_.empty()) {
+        throw std::runtime_error("diagnostic fields must be set before receiving values");
       }
 
-    private:
-      /**
-       * Output queue
-       */
+      stan::WriterMessage writer_message;
+      writer_message.set_topic(stan::WriterMessage_Topic_DIAGNOSTIC);
+      for (std::size_t i = 0; i < diagnostic_fields_.size(); ++i) {
+        stan::WriterMessage_Feature * feature = writer_message.add_feature();
+        feature->set_name(diagnostic_fields_[i]);
+        stan::WriterMessage_DoubleList * double_list = new stan::WriterMessage_DoubleList;
+        double_list->add_value(state[i]);
+        feature->set_allocated_double_list(double_list);
+      }
 
-      boost::lockfree::spsc_queue<std::string> * output_;
+      std::string serialized;
+      writer_message.SerializeToString(&serialized);
+      output_->push(serialized);
+      return;
+    } else if (message_prefix_ == "init_writer:") {
+      stan::WriterMessage writer_message;
+      writer_message.set_topic(stan::WriterMessage_Topic_INITIALIZATION);
 
-      /**
-       * Channel name with which to prefix strings added to the queue.
-       */
-      std::string message_prefix_;
-      std::vector<std::string> diagnostic_fields_;
-      std::vector<std::string> sample_fields_;
-      ProcessingAdaptationState processing_adaptation_state_ = ProcessingAdaptationState::BEFORE_PROCESSING_ADAPTATION;
-    };
+      stan::WriterMessage_Feature * feature = writer_message.add_feature();
+      stan::WriterMessage_DoubleList * double_list = new stan::WriterMessage_DoubleList;
+      for (std::vector<double>::const_iterator it = state.begin();
+          it != last; ++it) {
+        double_list->add_value(*it);
+      }
+      feature->set_allocated_double_list(double_list);
+      std::string serialized;
+      writer_message.SerializeToString(&serialized);
+      output_->push(serialized);
+      return;
+    } else if (message_prefix_ == "sample_writer:") {
+      if (sample_fields_.empty())
+        throw std::runtime_error("Sample fields should be populated before sample writer writes a vector of doubles.");
 
+      if ((processing_adaptation_state_ == ProcessingAdaptationState::PROCESSING_ADAPTATION) ||
+          (processing_adaptation_state_ == ProcessingAdaptationState::FINAL_ADAPTATION_MESSAGE))
+        throw std::runtime_error("Adaptation should have completed before sample writer writes a vector of doubles.");
+
+      stan::WriterMessage writer_message;
+      writer_message.set_topic(stan::WriterMessage_Topic_SAMPLE);
+
+      for (std::size_t i = 0; i < sample_fields_.size(); ++i) {
+        stan::WriterMessage_Feature * feature = writer_message.add_feature();
+        feature->set_name(sample_fields_[i]);
+        stan::WriterMessage_DoubleList * double_list = new stan::WriterMessage_DoubleList;
+        double_list->add_value(state[i]);
+        feature->set_allocated_double_list(double_list);
+      }
+      std::string serialized;
+      writer_message.SerializeToString(&serialized);
+      output_->push(serialized);
+      return;
+    }
   }
-}
-#endif
+
+  /**
+   * Writes the message_prefix to the stream followed by a newline.
+   */
+  void operator()() {
+    // unused
+    return;
+  }
+
+  /**
+   * Writes the message_prefix then the message.
+   *
+   * @param[in] message A string
+   */
+  void operator()(const std::string& message) {
+    if (message_prefix_ == "diagnostic_writer:") {
+      // DEBUG: prototype here
+      stan::WriterMessage writer_message;
+      writer_message.set_topic(stan::WriterMessage_Topic_DIAGNOSTIC);
+
+      stan::WriterMessage_Feature * feature = writer_message.add_feature();
+      stan::WriterMessage_StringList * string_list = new stan::WriterMessage_StringList;
+      string_list->add_value(message);
+      feature->set_allocated_string_list(string_list);
+
+      std::string serialized;
+      writer_message.SerializeToString(&serialized);
+      output_->push(serialized);
+      return;
+    } else if (message_prefix_ == "init_writer:") {
+      throw std::runtime_error("Unexpected string vector for init writer.");
+    } else if (message_prefix_ == "sample_writer:") {
+      // state machine dance here
+      if (processing_adaptation_state_ == ProcessingAdaptationState::BEFORE_PROCESSING_ADAPTATION) {
+        if (message.rfind("Adaptation terminated", 0) == 0) {
+          // message starts with "Adaptation terminated"
+          processing_adaptation_state_ = ProcessingAdaptationState::PROCESSING_ADAPTATION;
+        }
+      } else if (processing_adaptation_state_ == ProcessingAdaptationState::PROCESSING_ADAPTATION) {
+        if (message.rfind("Diagonal elements of inverse mass matrix", 0) == 0) {
+          // message starts with "Diagonal elements of inverse mass matrix"
+          // the next "message" (vector of doubles) will be the final adaptation message
+          processing_adaptation_state_ = ProcessingAdaptationState::FINAL_ADAPTATION_MESSAGE;
+        }
+      } else if (processing_adaptation_state_ == ProcessingAdaptationState::FINAL_ADAPTATION_MESSAGE) {
+        // this message is the last adaptation-related message before normal draws start arriving
+        processing_adaptation_state_ = ProcessingAdaptationState::AFTER_PROCESSING_ADAPTATION;
+      }
+
+      stan::WriterMessage writer_message;
+      writer_message.set_topic(stan::WriterMessage_Topic_SAMPLE);
+
+      stan::WriterMessage_Feature * feature = writer_message.add_feature();
+      stan::WriterMessage_StringList * string_list = new stan::WriterMessage_StringList;
+      string_list->add_value(message);
+      feature->set_allocated_string_list(string_list);
+
+      std::string serialized;
+      writer_message.SerializeToString(&serialized);
+      output_->push(serialized);
+      return;
+    }
+  }
+
+ private:
+  /**
+   * Output queue
+   */
+
+  boost::lockfree::spsc_queue<std::string> * output_;
+
+  /**
+   * Channel name with which to prefix strings added to the queue.
+   */
+  std::string message_prefix_;
+  std::vector<std::string> diagnostic_fields_;
+  std::vector<std::string> sample_fields_;
+  ProcessingAdaptationState processing_adaptation_state_ = ProcessingAdaptationState::BEFORE_PROCESSING_ADAPTATION;
+};
+
+}  // namespace callbacks
+}  // namespace stan
+#endif  // HTTPSTAN_QUEUE_WRITER_HPP
