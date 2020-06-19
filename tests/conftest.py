1935206d007d7d137b53bfddb3493ab570c170f5
Allen Riddell <riddella+stan@indiana.edu>
2020-06-18 21:05:46 -0400

    test: Simplify test fixtures
    Big simplification of test setup thanks to pytest-asyncio.
    No need for running the server in a separate thread.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,4 @@
 """pytest configuration for all tests."""
-import asyncio
-import threading
 import typing
 
 import aiohttp.web
@@ -10,7 +8,7 @@ import httpstan.app
 
 
 @pytest.fixture
-def server_host_port_pair(unused_tcp_port: int,) -> typing.Generator[typing.Tuple[str, int], None, None]:
+async def server_host_port_pair(unused_tcp_port: int,) -> typing.AsyncGenerator[typing.Tuple[str, int], None]:
     """Arrange event loop with httpstan server running.
 
     HTTP server shutdown is handled as well.
@@ -18,24 +16,14 @@ def server_host_port_pair(unused_tcp_port: int,) -> typing.Generator[typing.Tupl
     host, port = "127.0.0.1", unused_tcp_port
     app = httpstan.app.make_app()
     runner = aiohttp.web.AppRunner(app)
-    # After dropping Python 3.6, use `asyncio.run`
-    asyncio.get_event_loop().run_until_complete(runner.setup())
+    await runner.setup()
     site = aiohttp.web.TCPSite(runner, host, port)
-    # After dropping Python 3.6, use `asyncio.run`
-    asyncio.get_event_loop().run_until_complete(site.start())
-    # After dropping Python 3.6, use `get_running_loop`
-    loop = asyncio.get_event_loop()
-    t = threading.Thread(target=loop.run_forever)
-    # after this call, the event loop is running in thread which is not the main
-    # thread. All interactions with the event loop must use thread-safe calls
-    t.start()
+    await site.start()
     yield (host, port)
-    asyncio.run_coroutine_threadsafe(runner.cleanup(), loop)
-    loop.call_soon_threadsafe(loop.stop)  # stops `run_forever`
-    t.join(timeout=1)
+    await runner.cleanup()
 
 
 @pytest.fixture
-def api_url(server_host_port_pair: typing.Tuple[str, int]) -> str:
+async def api_url(server_host_port_pair: typing.Tuple[str, int]) -> str:
     host, port = server_host_port_pair
     return f"http://{host}:{port}/v1"
