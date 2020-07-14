fa3ae79ff04042c2aedb0ef3ac24300bcd2ec959
Allen Riddell <riddella+stan@indiana.edu>
2020-07-13 20:27:24 -0400

    feat: Remove Cython build dependency
    Remove inconvenient build-time dependency on Cython.
    Cython is used by httpstan to wrap the model-specific stan::services
    functions. Cython was also used to build a module "empty" whose
    only purpose was to force the Python wheel building tools to
    recognize the httpstan wheel as platform-specific (which it is due
    to the presence of shared libraries in `httpstan/lib`).
    
    Closes #399

---
+++ httpstan/empty.cpp
@@ -0,0 +1,17 @@
+#include <pybind11/pybind11.h>
+
+void noop() {
+    return;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(empty, m) {
+    m.doc() = R"pbdoc(
+        Empty extension module.
+
+        This module contains a single no-op function. It is compiled so that the wheel
+        machinery recognizes the wheel as being a platform-specific wheel.
+    )pbdoc";
+    m.def("noop", &noop, "");
+}
