a326f38e29b8825ef1466a2a5ed090997a03fed3
Allen Riddell <riddella+stan@indiana.edu>
2021-02-24 20:27:55 -0500

    fix: Get version from importlib
    Use importlib to get version. Same approach used in PyStan.

--- httpstan/__init__.py
+++ httpstan/__init__.py
@@ -4,7 +4,13 @@ Configures logging and exposes httpstan.__version__.
 
 :license: ISC, see LICENSE for more details.
 """
+import importlib.metadata
 import logging
 
 logging.getLogger("httpstan").addHandler(logging.NullHandler())
-__version__ = "4.7.1"
+
+# try-except allows mypy to run without httpstan being installed
+try:
+    __version__ = importlib.metadata.version("httpstan")
+except importlib.metadata.PackageNotFoundError:
+    pass
--- tests/test_httpstan.py
+++
@@ -1,5 +0,0 @@
-from httpstan import __version__
-
-
-def test_version() -> None:
-    assert __version__ is not None
