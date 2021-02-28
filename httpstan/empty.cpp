b63552adf824c3f6b56c8b8af0c987c2dbe2ab99
Allen Riddell <riddella+stan@indiana.edu>
2021-02-28 18:55:25 -0500

    style: clang-format C++ files
    This commit formats C++ files with:
    
    clang-format -i -style="{ColumnLimit: 119}"

--- httpstan/empty.cpp
+++ httpstan/empty.cpp
@@ -1,17 +1,15 @@
 #include <pybind11/pybind11.h>
 
-void noop() {
-    return;
-}
+void noop() { return; }
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(empty, m) {
-    m.doc() = R"pbdoc(
+  m.doc() = R"pbdoc(
         Empty extension module.
 
         This module contains a single no-op function. It is compiled so that the wheel
         machinery recognizes the wheel as being a platform-specific wheel.
     )pbdoc";
-    m.def("noop", &noop, "");
+  m.def("noop", &noop, "");
 }
