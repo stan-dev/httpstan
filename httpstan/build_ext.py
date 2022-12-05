5aee736e48f0fd4d02fac0248969d90c02c95011
A. Riddell <riddella+stan@indiana.edu>
2022-12-05 15:50:56 +0000

    fix: Finish migration to setuptools
    Finish migration from distutils to setuptools. Distutils
    is being deprecated by Python.
    
    There are minor differences between setuptools and distutils. These
    differences are not well-documented.
    
    This change appears to make things work.
    
    A better approach would be to subclass build_ext, following the
    guidelines in the setuptools documentation:
    
    https://setuptools.pypa.io/en/latest/userguide/extension.html
    
    Closes #631

--- httpstan/build_ext.py
+++ httpstan/build_ext.py
@@ -1,15 +1,15 @@
 """Lightly modified build_ext which captures stderr.
 
+The customization of build_ext here is non-standard and confusing.
+It does, however, work.
+
 isort:skip_file
 """
 
-# IMPORTANT: `import setuptools` MUST come before any module imports `distutils`
-# background: https://bugs.python.org/issue23102
-import setuptools  # noqa: F401
-
-import setuptools._distutils.command.build_ext as build_ext
-import setuptools._distutils.core
+import setuptools
+import setuptools.command.build_ext as build_ext
 import io
+import logging
 import os
 import sys
 import tempfile
@@ -18,18 +18,7 @@ from typing import IO, Any, List, TextIO
 from httpstan.config import HTTPSTAN_DEBUG
 
 
-def _get_build_extension() -> build_ext.build_ext:  # type: ignore
-    if HTTPSTAN_DEBUG:  # pragma: no cover
-        setuptools._distutils.log.set_verbosity(setuptools._distutils.log.DEBUG)  # type: ignore
-    dist = setuptools._distutils.core.Distribution()
-    # Make sure build respects distutils configuration
-    dist.parse_config_files(dist.find_config_files())  # type: ignore
-    build_extension = build_ext.build_ext(dist)  # type: ignore
-    build_extension.finalize_options()
-    return build_extension
-
-
-def run_build_ext(extensions: List[setuptools._distutils.core.Extension], build_lib: str) -> str:
+def run_build_ext(extensions: List[setuptools.Extension], build_lib: str) -> str:
     """Configure and call `build_ext.run()`, capturing stderr.
 
     Compiled extension module will be placed in `build_lib`.
@@ -64,7 +53,14 @@ def run_build_ext(extensions: List[setuptools._distutils.core.Extension], build_
         os.dup2(stream.fileno(), stderr_fileno)
         return orig_stderr
 
-    build_extension = _get_build_extension()
+    if HTTPSTAN_DEBUG:  # pragma: no cover
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    dist = setuptools.Distribution()
+    # Make sure build respects distutils configuration
+    dist.parse_config_files(dist.find_config_files())  # type: ignore
+    build_extension = build_ext.build_ext(dist)  # type: ignore
+
     build_extension.build_lib = build_lib
 
     # silence stderr for compilation, if stderr is silenceable
@@ -74,6 +70,16 @@ def run_build_ext(extensions: List[setuptools._distutils.core.Extension], build_
     if redirect_stderr:
         orig_stderr = _redirect_stderr_to(stream)
 
+    # NOTE: work-around for differences between setuptools and distutils. A bit of a hack.
+    # The sequence here is important. `finalize_options` needs to be called first.
+    # A cleaner approach would make a custom command, following instructions here:
+    # https://setuptools.pypa.io/en/latest/userguide/extension.html
+    build_extension.finalize_options()
+    for extension in extensions:
+        # WISHLIST: understand setuptools internals enough to know what this is
+        # and why setting this here is required when using setuptools but not
+        # when using distutils.
+        extension._needs_stub = False
     build_extension.extensions = extensions
 
     try:
